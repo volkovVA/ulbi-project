b9b8511602182fa6260c84a15758a7a33a8ac8c6
Volkov Viacheslav <vvolkov5@alfabank.ru>
2023-02-02 23:43:26 +0300

    refactor(1-9): change the code to FSD architectural methodology

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true, // Настройка абс.путей: абсолютные пути в приоритете
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'], // Настройка абс.путей: для каждого модуля главным файлом будет index
+        alias: {} // Настройка абс.путей: alias указывает как будет выглядеть импорт. Может начинаться с @ или без знака
     }
 }
