f587d550b3b5e717055609013b022929664093a3
Volkov Viacheslav <vvolkov5@alfabank.ru>
2023-02-02 20:51:07 +0300

    feat(1-8): add classNames helpers utilite

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
+
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
