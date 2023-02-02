d533999b70402f5bbf765eaa988765285e4958ce
Volkov Viacheslav <vvolkov5@alfabank.ru>
2023-02-02 16:26:50 +0300

    feat: add themes, organize styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import {Link, Route, Routes} from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
