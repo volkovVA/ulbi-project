237eb79239113971e6d3b9f0e9e5f7ecc87a7e30
Volkov Viacheslav <vvolkov5@alfabank.ru>
2023-05-20 18:51:07 +0300

    feat(2-21): add page loader & loader

--- src/app/providers/router/component/AppRouter.tsx
+++ src/app/providers/router/component/AppRouter.tsx
@@ -1,24 +1,24 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => (
-    // eslint-disable-next-line i18next/no-literal-string
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
+    <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
