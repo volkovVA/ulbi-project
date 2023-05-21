eab01626924dec87edcb1891fcbf0e4c0e8de430
Volkov Viacheslav <vvolkov5@alfabank.ru>
2023-05-21 16:20:41 +0300

    feat(3-23): add BundleAnalyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': 'off',
+        'max-len': ['error', { ignoreCommetns: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
